6c2e953a004a5244b70150b0c83027ddb245f6be
LucasLiu3 <Hao.Liu@lincolnuni.ac.nz>
2024-08-01 11:34:01 +1200

    set up

---
+++ Full Stack Project/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello Full stack project!
+      </h1>
+    </div>
+  );
+}
